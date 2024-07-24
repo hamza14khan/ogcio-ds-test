a696add00371b066a8903f4caa6b14f74361cc6e
hamza14khan <hamzaubit14@gmail.com>
2024-07-24 18:11:37 +0100

    poc: html multi page

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,8 +1,38 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
+import htmlConfig from "vite-plugin-html-config";
 
 export default defineConfig({
   base: "./",
+  plugins: [
+    htmlConfig({
+      pages: {
+        main: {
+          entry: "main.js",
+          template: "index.html",
+          filename: "index.html",
+        },
+        agriculture: {
+          entry: "pages/agriculture/main.js",
+          template: "pages/agriculture/index.html",
+          filename: "pages/agriculture/index.html",
+        },
+        search: {
+          entry: "pages/search/main.js",
+          template: "pages/search/index.html",
+          filename: "pages/search/index.html",
+        },
+      },
+    }),
+  ],
   build: {
     minify: "esbuild",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        agriculture: resolve(__dirname, "pages/agriculture/index.html"),
+        search: resolve(__dirname, "pages/search/index.html"),
+      },
+    },
   },
 });
