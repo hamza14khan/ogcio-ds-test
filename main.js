4c9d8ee32a0e6ea2f35ac75ebbcc295311bd37c1
hamza14khan <hamzaubit14@gmail.com>
2024-07-24 18:03:21 +0100

    poc: page fixes

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 import "./style.scss";
 import { initAll } from "@ogcio/ogcio-ds";
 
+document.addEventListener("DOMContentLoaded", function () {
+  const contentWrapper = document.querySelector(".poc-govie");
+  contentWrapper.style.display = "block"; // Show content when DOM is loaded
+});
+
 initAll();
